6c68de808f2d5aaee086c1604842d4a54008cf40
Aaron00101010 <aaron00101010@gmail.com>
2022-01-12 20:08:46 +0800

    chore: add manifest.json

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
 import nodeResolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import html from '@web/rollup-plugin-html';
+import copy from 'rollup-plugin-copy'
 import { importMetaAssets } from '@web/rollup-plugin-import-meta-assets';
 import { terser } from 'rollup-plugin-terser';
-import { generateSW } from 'rollup-plugin-workbox';
-import path from 'path';
+
 
 export default {
   input: 'index.html',
@@ -20,8 +20,8 @@ export default {
   plugins: [
     /** Enable using HTML as rollup entrypoint */
     html({
-      minify: true,
-      injectServiceWorker: true,
+      minify: false,
+      injectServiceWorker: false,
       serviceWorkerPath: 'dist/sw.js',
     }),
     /** Resolve bare module imports */
@@ -66,19 +66,10 @@ export default {
         ],
       ],
     }),
-    /** Create and inject a service worker */
-    generateSW({
-      globIgnores: ['polyfills/*.js', 'nomodule-*.js'],
-      navigateFallback: '/index.html',
-      // where to output the generated sw
-      swDest: path.join('dist', 'sw.js'),
-      // directory to match patterns against to be precached
-      globDirectory: path.join('dist'),
-      // cache any html js and css by default
-      globPatterns: ['**/*.{html,js,css,webmanifest}'],
-      skipWaiting: true,
-      clientsClaim: true,
-      runtimeCaching: [{ urlPattern: 'polyfills/*.js', handler: 'CacheFirst' }],
-    }),
+    copy({
+      targets: [
+        { src: "manifest.json", dest: "dist" }
+      ]
+    })
   ],
 };
